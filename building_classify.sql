06cc96605812f9bb4e527998728573f5f16ed6e3
Felis Pimeja <felispimeja+github.com@gmail.com>
2020-09-03 00:25:50 +0300

    Update building_classify.sql

--- building_classify.sql
+++ building_classify.sql
@@ -7,200 +7,225 @@
 -- 3. Переработать классы на основе МинЖКХ - отсутствие этажности хреновый признак для распознания ИЖС
 -- 4. Нижний Новгород - сараи в МКД кварталах зачем-то распознаются как ИЖС!
 
-/* буферы от точек dom.mingkh.ru  + id_gis */
-drop table if exists building_min_gkh_buffer;
-create temp table building_min_gkh_buffer as
-select m.*, st_buffer(m.geom::geography, 5)::geometry buffer
-from russia.building_mingkh m
-join index2019.data_boundary b
-	on st_intersects(m.geom, b.geom)
-where m.id_gis = 1080 -- дебаг
+/* !!! дебаг - задаём город !!! */
+drop table if exists city;
+create temp table city as
+select id_gis::smallint, geom from index2019.data_boundary
+--where id_gis = 1084 -- дебаг
 ;
-create index on building_min_gkh_buffer (id_gis);
-create index on building_min_gkh_buffer using gist(buffer);
+create index on city(id_gis);
+create index on city using gist(geom);
+
+/* предварительная подготовка данных МинЖКХ */
+drop table if exists mingkh;
+create temp table mingkh as
+select
+	m.id,
+	m.id_gis,
+	case 
+		when m.square in ('Не заполнено', 'Незаполнено', '')
+			or m.square::real = 0
+			then null
+		else round(m.square::numeric)::int
+	end square,
+	case 
+		when m.year in ('Не заполнено', 'Незаполнено', '')
+			then null
+		else m.year::smallint
+	end built_year,
+	case 
+		when m.floors in ('Не заполнено', 'Незаполнено', '')
+			or m.floors::int = 0
+			then null
+		else m.floors::smallint
+	end floor,
+	m.geom
+from city c
+join russia.building_mingkh m using(id_gis);
+
+create index on mingkh(id_gis);
+create index on mingkh(built_year);
+create index on mingkh(floor);
+create index on mingkh using gist(geom);
+create index on mingkh using gist((geom::geography));
 
 --explain
 /* классификация начинается здесь */
-drop table if exists building3;
-create temp table building3 as 
-
-with
 /* первая итерация - классификация по OpenStreetMap */
-building1 as (
-	select distinct on(b.id)
-		b.id,
-		b.id_gis,
-		b.type osm_type,
-		case
-			when b.type in ('apartments', 'dormitory') or (b.type = 'residential' and b.levels > 1)
-				then 'mkd'::varchar
-			else case
-				when b.type in ('house', 'detached') or (b.type = 'residential' and b.levels < 2)
-					then 'igs'::varchar
---					then 'residential'::varchar
-				when b.type = 'yes' then 'other'::varchar
-				else 'other'::varchar
-			end
-		end building_type,
-		null::smallint build_year,
-		case
-			when b.levels is not null
-				then b.levels::smallint
-			when b.levels is null
-				and b.type in ('garage', 'garages', 'fuel', 'greenhouse', 'hangar', 'hut', 'kiosk', 'roof', 'service', 'shed', 'stable', 'tank')
-				then 1::smallint
-			else null::smallint
-		end levels,
-		b.geom
-	from index2019.data_building b
-	where id_gis = 1080 -- дебаг
-),
+drop table if exists building1;
+create temp table building1 as 
+select 
+	b.id,
+	b.id_gis,
+	b.type osm_type,
+	case
+		when
+			(b.type in ('apartments', 'dormitory') and st_area(b.geom::geography) > 550)
+				or (b.type in ('house', 'residential') and b.levels > 2)
+			then 'mkd'::varchar
+		else case
+			when b.type in ('house', 'detached', 'residential') and b.levels < 3
+				then 'igs'::varchar
+			else 'other'::varchar
+		end
+	end building_type,
+	case
+		when b.levels is not null
+			then b.levels::smallint
+		when b.levels is null
+			and b.type in ('garage', 'garages', 'fuel', 'greenhouse', 'hangar', 'hut', 'kiosk', 'roof', 'service', 'shed', 'stable', 'tank')
+			then 1::smallint
+		else null::smallint
+	end levels,
+	b.geom
+from city c
+join index2019.data_building b using(id_gis);
+
+create index on building1(id_gis);
+create index on building1(building_type);
+create index on building1 using gist(geom);
+create index on building1 using gist((geom::geography));
 
 /* вторая итерация - уточнение по данным dom.mingkh.ru */
-building2 as (
-	select 
-		b.id,
-		b.id_gis,
-		b.osm_type,
-		case
-			when b.building_type = 'other'
-				and max(mb.id) is not null
-				and case
-					when max(mb.floors) != 'Не заполнено'
-						and max(mb.floors) not like '% %' -- потому что в МинЖКХ была лажа
-						then max(mb.floors)::smallint
-				end > 1
-				then 'mkd'::varchar
-			when b.building_type = 'other'
-				and max(mb.id) is not null
-				and case
-					when max(mb.floors) != 'Не заполнено'
-						and max(mb.floors) not like '% %'
-						then max(mb.floors)::smallint
-					when max(mb.floors)  = 'Не заполнено'
-						or max(mb.floors) like '% %'
-						then 1::smallint
-				end = 1
-				then 'igs'::varchar
-			else b.building_type
-		end building_type,
-		case
-			when b.build_year is null
-				and min(mb.year) not in ('Не заполнено','0','1')
-				then min(mb.year)::smallint
-			else b.build_year
-		end build_year,
-		case 
-			when max(mb.floors) is not null
-				and max(mb.floors) != 'Не заполнено'
-				and max(mb.floors) not like '% %'
-				then max(mb.floors)::smallint
-			else b.levels::smallint
-		end levels,
-		b.geom
-	from building1 b
-	left join building_min_gkh_buffer mb
-		on b.id_gis = mb.id_gis
-			and st_intersects(b.geom, mb.buffer)
-	group by b.id, b.id_gis, b.osm_type, b.building_type, b.build_year, b.levels, b.geom
-),
+drop table if exists building2;
+create temp table building2 as
+select 
+	b.id,
+	b.id_gis,
+	b.osm_type,
+	case
+		when b.building_type = 'other'
+			and mb.id is not null
+			then 'mkd'::varchar
+		else b.building_type
+	end building_type,
+	case
+		when mb.built_year is not null
+			then mb.built_year
+		else null
+	end built_year,
+	case 
+		when mb.floor is not null
+			then mb.floor
+		else b.levels
+	end levels,
+	b.geom
+from building1 b
+left join lateral (
+	select *
+	from mingkh m
+	where b.id_gis = m.id_gis
+		and st_dwithin(b.geom::geography, m.geom::geography, 5)
+	order by m.geom::geography <-> b.geom::geography
+	limit 1
+) mb on true;
+
+create index on building2(id_gis);
+create index on building2(building_type);
+create index on building2(levels);
+create index on building2 using gist(geom);
+create index on building2 using gist((geom::geography));
 
 /* третья итерация - уточнение по данным Альтермага */
-building3 as (
-	select 
-		b.id,
-		b.id_gis,
-		b.osm_type,
-		case
-			when b.building_type = 'other'
-				and max(a.id) is not null
-				then 'mkd'
-			else b.building_type
-		end building_type,
-		b.build_year,
-		sum(a.population) population,
-		case
-			when (b.levels is null and max(a.id) is not null)
-				or max(a.floor) > b.levels
-				then max(a.floor)::smallint
-			else b.levels
-		end levels,
-		st_area(b.geom::geography)::int area_m2,
-		b.geom
-	from building2 b
-	left join index2019.data_pop_altermag a
-		on a.id_gis = b.id_gis
-			and st_intersects(a.geom, b.geom)
-			and a.floor > 1
-	group by b.id, b.id_gis, b.osm_type, b.building_type, b.build_year, b.levels, b.geom
-)
+drop table if exists building3;
+create temp table building3 as
+select 
+	b.id,
+	b.id_gis,
+	b.osm_type,
+	case
+		when b.building_type = 'other'
+			and a.floor is not null
+			then 'mkd'
+		else b.building_type
+	end building_type,
+	b.built_year,
+	a.population,
+	case
+		when (b.levels is null and a.floor is not null)
+			or a.floor > b.levels
+			then a.floor::smallint
+		else b.levels
+	end levels,
+	st_area(b.geom::geography)::int area_m2,
+	b.geom
+from building2 b
+left join lateral (
+	select a.floor, a.population, a.id_gis, a.geom
+	from index2019.data_pop_altermag a
+	where a.id_gis = b.id_gis
+		and a.floor > 1
+		and st_dwithin(a.geom::geography, b.geom::geography, 5)
+	order by a.geom::geography <-> b.geom::geography
+	limit 1
+) a on true;
 
-select * from building3;
 create index on building3(id_gis);
+create index on building3(area_m2);
 create index on building3 using gist(geom);
 
-drop table if exists building4;
-create table building4 as 
+drop table if exists landuse;
+create temp table landuse as 
+select l.*, c.id_gis
+from city c
+join osm.landusages_ru l
+	on st_intersects(c.geom, l.geom)
+		and l.type in ('residential', 'allotments');
 
-with
-/* отбор кварталов landuse = residential по выгрузке OpenStreetMap */
-landuse1 as (
-	select l.*, b.id_gis
-	from index2019.data_boundary b
-	join osm.landusages_ru l
-		on st_intersects(b.geom, l.geom)
-			and l.type in ('residential', 'allotments')
-	where b.id_gis = 1080 -- дебаг
-),
+create index on landuse(id_gis);
+create index on landuse using gist(geom);
 
 /* подсчёт средней площади здания на квартал landuse = residential (поиск кварталов ИЖС) */
-landuse2 as (
-	select l.*, avg(area_m2) avg_build_area
-	from landuse1 l
-	join building3 b
-		on b.id_gis = l.id_gis
-			and st_intersects(l.geom, b.geom)
---			and b.building_type <> 'other'
-	group by l.id, l.id_gis, l.name, l.type, l.geom
-),
+drop table if exists landuse2;
+create temp table landuse2 as 
+select l.id, l.id_gis, l.type, l.geom, avg(area_m2) avg_build_area
+from landuse l
+join building3 b
+	on b.id_gis = l.id_gis
+		and st_intersects(l.geom, b.geom)
+group by l.id, l.id_gis, l.type, l.geom;
+
+create index on landuse2(id_gis);
+create index on landuse2(type);
+create index on landuse2 using gist(geom);
 
 /* четвёртая итерация - уточнение ИЖС по средней площади здания на квартал и площади здания */
-building4 as (
-	select distinct on(b.id)
-		b.id,
-		b.id_gis,
-		b.osm_type,
-		case
-			when b.building_type = 'other'
-				then case
-					when l.id is not null
-						and avg_build_area <= 200
-						and st_area(b.geom::geography, true) <= 550
---						then 'residential'
-						then 'igs'
-					else b.building_type
-				end
-			else b.building_type
-		end building_type,
-		b.build_year,
-		b.population,
-		b.levels,
-		b.area_m2,
-		b.geom
-	from building3 b
-	left join landuse2 l
-		on b.id_gis = l.id_gis
-			and st_intersects(b.geom, l.geom)
-			and l.type in ('residential', 'allotments')
-)
+drop table if exists building4;
+create temp table building4 as
+select distinct --on(b.id)
+	b.id,
+	b.id_gis,
+	b.osm_type,
+	case
+		when b.building_type = 'other'
+			then case
+				when l.id is not null
+					and avg_build_area <= 200
+					and st_area(b.geom::geography, true) <= 550
+					then 'igs'
+				else b.building_type
+			end
+		else b.building_type
+	end building_type,
+	b.built_year,
+	b.population,
+	b.levels,
+	b.area_m2,
+	b.geom
+from landuse2 l
+join building3 b
+	on b.id_gis = l.id_gis
+		and st_intersects(b.geom, l.geom)
+where l.type in ('residential', 'allotments');
+
 
 select * from building4;
 create index on building4(id_gis);
 create index on building4 using gist(geom);
 
 
-drop table if exists russia.building_classify;
-create table russia.building_classify as 
+drop table if exists russia.building_classify2;
+create table russia.building_classify2 as 
 
 with
 /* фильтрация памятников  */
@@ -239,19 +264,19 @@ building5 as (
 select * from building5;
 
 /* первичный ключ и индексы */
-alter table tmp.building_classified_1080 drop column id;
-alter table tmp.building_classified_1080 add column id serial primary key;
-create index on tmp.building_classified_1080 using gist(geom);
-create index on tmp.building_classified_1080 (levels);
-create index on tmp.building_classified_1080 (build_year);
-create index on tmp.building_classified_1080 (building_type);
+alter table russia.building_classify2 drop column id;
+alter table russia.building_classify2 add column id serial primary key;
+create index on russia.building_classify2 using gist(geom);
+create index on russia.building_classify2 (levels);
+create index on russia.building_classify2 (built_year);
+create index on russia.building_classify2 (building_type);
 
 /* комментарии */
-comment on table tmp.building_classified_1080 is 'Типология зданий (жилые/нежилые) + этажность и год постройки. Источники: OpenStreetMap, Альтермаг, dom.gosuslugi.ru и dom.mingkh.ru';
-comment on column tmp.building_classified_1080.id is 'Первичный ключ';
-comment on column tmp.building_classified_1080.id_gis is 'id_gis города';
-comment on column tmp.building_classified_1080.osm_type is 'Тип здания по OpenStreetMap';
-comment on column tmp.building_classified_1080.building_type is 'Тип здания (МКД/ИЖС/Прочее)';
-comment on column tmp.building_classified_1080.build_year is 'Год постройки';
-comment on column tmp.building_classified_1080.levels is 'Количество этажей';
-comment on column tmp.building_classified_1080.geom is 'Геометрия';
+comment on table russia.building_classify2 is 'Типология зданий (жилые/нежилые) + этажность и год постройки. Источники: OpenStreetMap, Альтермаг, dom.gosuslugi.ru и dom.mingkh.ru';
+comment on column russia.building_classify2.id is 'Первичный ключ';
+comment on column russia.building_classify2.id_gis is 'id_gis города';
+comment on column russia.building_classify2.osm_type is 'Тип здания по OpenStreetMap';
+comment on column russia.building_classify2.building_type is 'Тип здания (МКД/ИЖС/Прочее)';
+comment on column russia.building_classify2.built_year is 'Год постройки';
+comment on column russia.building_classify2.levels is 'Количество этажей';
+comment on column russia.building_classify2.geom is 'Геометрия';
