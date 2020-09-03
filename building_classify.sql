4ece9f23c7dd79c29dd26af099eaec4dc30c82b1
Felis Pimeja <felispimeja+github.com@gmail.com>
2020-09-03 03:42:50 +0300

    Исправил ошибки для большей точности.

--- building_classify.sql
+++ building_classify.sql
@@ -1,17 +1,14 @@
 /* типология зданий на основе OpenStreetMap, Альтермага и dom.mingkh.ru */
-/* версия 2 */
+/* версия 3 */
 --/* время расчёта ~ 1.5 часа. на всю Россию */ ???
 /* to do: */
 -- 1. Убрать задублированную геометрию из OpenStreetMap (см. Челябинск - угловые дома в частном секторе)
--- 2. Проверять классы из OSM - там и лажа попадается - какая неожиданность!
--- 3. Переработать классы на основе МинЖКХ - отсутствие этажности хреновый признак для распознания ИЖС
--- 4. Нижний Новгород - сараи в МКД кварталах зачем-то распознаются как ИЖС!
 
 /* !!! дебаг - задаём город !!! */
 drop table if exists city;
 create temp table city as
 select id_gis::smallint, geom from index2019.data_boundary
---where id_gis = 1084 -- дебаг
+where id_gis = 1082 -- дебаг
 ;
 create index on city(id_gis);
 create index on city using gist(geom);
@@ -69,6 +66,7 @@ select
 			else 'other'::varchar
 		end
 	end building_type,
+	'OpenStreetMap'::varchar building_type_source,
 	case
 		when b.levels is not null
 			then b.levels::smallint
@@ -77,6 +75,11 @@ select
 			then 1::smallint
 		else null::smallint
 	end levels,
+	case 
+		when b.levels is not null 
+			then 'OpenStreetMap'::varchar
+		else null 
+	end levels_source,
 	b.geom
 from city c
 join index2019.data_building b using(id_gis);
@@ -100,15 +103,31 @@ select
 		else b.building_type
 	end building_type,
 	case
+		when b.building_type = 'other'
+			and mb.id is not null
+			then 'МинЖКХ'::varchar
+		else b.building_type_source
+	end building_type_source,
+	case
 		when mb.built_year is not null
 			then mb.built_year
 		else null
 	end built_year,
 	case 
+		when mb.built_year is not null 
+			then 'МинЖКХ'::varchar
+		else null 
+	end built_year_source,
+	case 
 		when mb.floor is not null
 			then mb.floor
 		else b.levels
 	end levels,
+	case 
+		when mb.floor is not null
+			then 'МинЖКХ'::varchar
+		else b.levels_source
+	end levels_source,
 	b.geom
 from building1 b
 left join lateral (
@@ -139,14 +158,32 @@ select
 			then 'mkd'
 		else b.building_type
 	end building_type,
+	case
+		when b.building_type = 'other'
+			and a.floor is not null
+			then 'Альтермаг'::varchar
+		else b.building_type_source
+	end building_type_source,
 	b.built_year,
+	b.built_year_source,
 	a.population,
+	case 
+		when a.population is not null 
+			then 'Альтермаг'::varchar 
+		else null 
+	end population_source,
 	case
 		when (b.levels is null and a.floor is not null)
 			or a.floor > b.levels
 			then a.floor::smallint
 		else b.levels
 	end levels,
+	case
+		when (b.levels is null and a.floor is not null)
+			or a.floor > b.levels
+			then 'Альтермаг'::varchar
+		else b.levels_source
+	end levels_source,
 	st_area(b.geom::geography)::int area_m2,
 	b.geom
 from building2 b
@@ -178,7 +215,13 @@ create index on landuse using gist(geom);
 /* подсчёт средней площади здания на квартал landuse = residential (поиск кварталов ИЖС) */
 drop table if exists landuse2;
 create temp table landuse2 as 
-select l.id, l.id_gis, l.type, l.geom, avg(area_m2) avg_build_area
+select
+	l.id,
+	l.id_gis,
+	l.type,
+	l.geom,
+	avg(b.area_m2) avg_build_area,
+	round((count(b.*) filter(where b.building_type = 'mkd')::numeric / count(b.*))::numeric, 1) mkd_proportion
 from landuse l
 join building3 b
 	on b.id_gis = l.id_gis
@@ -201,82 +244,96 @@ select distinct --on(b.id)
 			then case
 				when l.id is not null
 					and avg_build_area <= 200
+					and mkd_proportion <= 0.2
 					and st_area(b.geom::geography, true) <= 550
 					then 'igs'
 				else b.building_type
 			end
 		else b.building_type
 	end building_type,
+--	l.mkd_proportion, --дебаг
+	case
+		when b.building_type = 'other'
+			then case
+				when l.id is not null
+					and avg_build_area <= 200
+					and mkd_proportion <= 0.2
+					and st_area(b.geom::geography, true) <= 550
+					then 'Средняя площадь здания на квартал'::varchar
+				else b.building_type_source
+			end
+		else b.building_type_source
+	end building_type_source,
 	b.built_year,
+	b.built_year_source,
 	b.population,
+	b.population_source,
 	b.levels,
+	b.levels_source,
 	b.area_m2,
 	b.geom
-from landuse2 l
-join building3 b
+from building3 b
+left join landuse2 l
 	on b.id_gis = l.id_gis
-		and st_intersects(b.geom, l.geom)
-where l.type in ('residential', 'allotments');
+		and st_intersects(b.geom, l.geom);
 
-
-select * from building4;
 create index on building4(id_gis);
 create index on building4 using gist(geom);
 
-
-drop table if exists russia.building_classify2;
-create table russia.building_classify2 as 
-
-with
 /* фильтрация памятников  */
-okn_filtered as (
-	select
-		id,
-		id_gis,
-		nativename,
-		geom
-	from index2019.data_okn
-	where (
-		nativename !~* 'могил|ограда| стел+а|обелиск|мемориал|бюст|надгроб|склеп|знак|улица|статуя|урна|памятник|скульптур|место|стена|кладбище|мост|пруд|фонтан|участок'
-		or (nativename ~* 'жилой|дом|домик|флигель|башня|ансамбль|часовн|палаты|терем|павильон|школ|церковь'
-			and nativename !~* 'памятник|скульптур|место|стена|кладбище|мост|пруд|фонтан|участок')
-	) 
-		and id_gis = 1080 -- дебаг
-),
+drop table if exists okn_filtered;
+create temp table okn_filtered as
+select
+	o.nativeid,
+	o.id_gis,
+	o.nativename,
+	o.geom
+from city c
+left join index2019.data_okn o using(id_gis)
+where (o.nativename !~* 'могил|ограда| стел+а|обелиск|мемориал|бюст|надгроб|склеп|знак|улица|статуя|урна|памятник|скульптур|место|стена|кладбище|мост|пруд|фонтан|участок'
+	or (o.nativename ~* 'жилой|дом|домик|флигель|башня|ансамбль|часовн|палаты|терем|павильон|школ|церковь'
+		and o.nativename !~* 'памятник|скульптур|место|стена|кладбище|мост|пруд|фонтан|участок')
+);
+
+create index on okn_filtered(id_gis);
+create index on okn_filtered using gist(geom);
+create index on okn_filtered using gist((geom::geography));
+
 
 /* сопоставление зданий с ОКН из реестра минкульта */
-building5 as (
-	select distinct on(b.id)
-		b.*,
---		st_collect(o.geom, b.geom), -- для дебага
-		o.id okn_id
-	from building4 b
-	left join lateral (
-		select o.id
-		from okn_filtered o
-		where o.id_gis = b.id_gis
-			and st_dwithin(o.geom::geography, b.geom::geography, 10)
-		order by o.geom::geography <-> (st_centroid(b.geom))::geography
-		limit 1
-	) o on true
-)
+drop table if exists russia.building_classify;
+create table russia.building_classify as 
+select distinct on(b.id)
+	b.*,
+	o.okn_id
+from building4 b
+left join lateral (
+	select o.nativeid okn_id
+	from okn_filtered o
+	where o.id_gis = b.id_gis
+		and st_dwithin(o.geom::geography, b.geom::geography, 10)
+	order by o.geom::geography <-> (st_centroid(b.geom))::geography
+	limit 1
+) o on true;
 
-select * from building5;
 
 /* первичный ключ и индексы */
-alter table russia.building_classify2 drop column id;
-alter table russia.building_classify2 add column id serial primary key;
-create index on russia.building_classify2 using gist(geom);
-create index on russia.building_classify2 (levels);
-create index on russia.building_classify2 (built_year);
-create index on russia.building_classify2 (building_type);
+alter table russia.building_classify drop column id;
+alter table russia.building_classify add column id serial primary key;
+create index on russia.building_classify using gist(geom);
+create index on russia.building_classify (levels);
+create index on russia.building_classify (built_year);
+create index on russia.building_classify (building_type);
 
 /* комментарии */
-comment on table russia.building_classify2 is 'Типология зданий (жилые/нежилые) + этажность и год постройки. Источники: OpenStreetMap, Альтермаг, dom.gosuslugi.ru и dom.mingkh.ru';
-comment on column russia.building_classify2.id is 'Первичный ключ';
-comment on column russia.building_classify2.id_gis is 'id_gis города';
-comment on column russia.building_classify2.osm_type is 'Тип здания по OpenStreetMap';
-comment on column russia.building_classify2.building_type is 'Тип здания (МКД/ИЖС/Прочее)';
-comment on column russia.building_classify2.built_year is 'Год постройки';
-comment on column russia.building_classify2.levels is 'Количество этажей';
-comment on column russia.building_classify2.geom is 'Геометрия';
+comment on table russia.building_classify is 'Типология зданий (жилые/нежилые) + этажность и год постройки. Источники: OpenStreetMap, Альтермаг, dom.gosuslugi.ru и dom.mingkh.ru';
+comment on column russia.building_classify.id is 'Первичный ключ';
+comment on column russia.building_classify.id_gis is 'id_gis города';
+comment on column russia.building_classify.osm_type is 'Тип здания по OpenStreetMap';
+comment on column russia.building_classify.building_type is 'Тип здания (МКД/ИЖС/Прочее)';
+comment on column russia.building_classify.building_type_source is 'Источник типа здания';
+comment on column russia.building_classify.built_year is 'Год постройки';
+comment on column russia.building_classify.built_year_source is 'Источник года постройки';
+comment on column russia.building_classify.levels is 'Количество этажей';
+comment on column russia.building_classify.levels_source is 'Источник этажности';
+comment on column russia.building_classify.geom is 'Геометрия';
