c8b8124207758d3d0ea78bc04245df4eff79fc3e
FelisPimeja <felispimeja@gmail.com>
2020-09-08 16:54:25 +0300

    misc edits

--- Scripts/misc scripts.sql
+++ Scripts/misc scripts.sql
@@ -10,4 +10,18 @@ select
 	i.pop_instopsarea "Население в радиусе норматив. доступн. остановок ОТ",
 	round(i.i32::numeric * 100) "Процент населения в радиусе доступности ОТ"
 from veb_rf.city b
-left join index2019.ind_i32 i using(id_gis)
+left join index2019.ind_i32 i using(id_gis);
+
+
+-- Трамвайные линии для (Волгоград Нижний Новгород Санкт-Петербург (Красногвардейский раойн) Самара)
+select r.*, b.id_gis
+from russia.city_boundary b
+join osm.railroads_ru r
+	on st_intersects(r.geom, b.geom)
+		and r.type = 'tram'
+where b.city in (
+	'Волгоград',
+	'Нижний Новгород',
+	'Санкт-Петербург',
+	'Самара'
+)
