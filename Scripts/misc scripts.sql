e4002a3ed3512eec4fff6836ae3c940e9464660b
FelisPimeja <felispimeja@gmail.com>
2020-09-08 16:42:48 +0300

    misc scripts added

---
+++ Scripts/misc scripts.sql
@@ -0,0 +1,13 @@
+/* Здесь всё временное и проходное - "на всякий случай" */
+
+--доступность остановок общественного транспорта для городов ВЭБа
+select
+	(row_number() over(order by i.pop_total desc))::int id,	
+	b.id_gis,
+	b.city "Город",
+	b.region "Субъект РФ",
+	i.pop_total "Население города (Росстат 2019 г.)" ,
+	i.pop_instopsarea "Население в радиусе норматив. доступн. остановок ОТ",
+	round(i.i32::numeric * 100) "Процент населения в радиусе доступности ОТ"
+from veb_rf.city b
+left join index2019.ind_i32 i using(id_gis)
