be43170a8d640ab3b91149800ff92b80318aa04e
FelisPimeja <felispimeja@gmail.com>
2020-09-16 00:35:16 +0300

    misc edits

--- Scripts/GPS tracks analysis/gps_tracks_misc.sql
+++ Scripts/GPS tracks analysis/gps_tracks_misc.sql
@@ -5,7 +5,7 @@ select * from tmp.quazar_dataset
 select * from tmp.quazar_clipped
 
 create index on tmp.quazar_dataset using gist(wkb_geometry);
-create index on tmp.quazar_dataset((speed::int))
+create index on tmp.quazar_dataset((speed::int));
 
 drop table if exists tmp.quazar_clipped;
 create table tmp.quazar_clipped as
@@ -21,9 +21,28 @@ select
 from russia.city_boundary b
 join tmp.quazar_dataset q
 	on st_intersects(q.wkb_geometry, b.geom)
-		and q.speed::int between 0 and 140
+		and q.speed::int between 0 and 155
 ;
 
+
+-- Фикс для кривых данных со стороны Квазара:
+select q.ogc_fid id,
+	q.track_id::int,
+	to_timestamp(q.timestamp::int) date_time,
+	substring(z, 3, length(z))::int speed,
+	q.speed::int bearing,
+	q.bearing::smallint veh_type,
+	q.wkb_geometry::geometry(point, 4326) geom,
+	b.id_gis
+from russia.city_boundary b
+join tmp.quazar_dataset q
+	on st_intersects(q.wkb_geometry, b.geom)
+		and substring(z, 3, length(z))::int between 10 and 155
+where b.id_gis = 1051;
+
+
+
+-- Проверка фильтра для qgis (т.к. он не умеет в специфические для pg касты и функции)
 select *, cast(substr(cast(cast("date_time" as time) as varchar), 1, 2) as int) time_ from tmp.quazar_clipped limit 100
 
 select count(q.*) from veb_rf.city c join tmp.quazar_clipped q using(id_gis)
@@ -72,21 +91,23 @@ group by track_id;
 
 
 -- Сажаем треки на граф (присваиваем id ближайшей дороги)
-drop table if exists tmp.quazar_veb_on_track;
-create table tmp.quazar_veb_on_track as
+drop table if exists tmp.quazar_on_track;
+create table tmp.quazar_on_track as
 select
 	q.*,
 	r.street_id
-from tmp.quazar_veb q
+from tmp.quazar_clipped q
 left join lateral (
 	select street_id, geom
 	from index2019.data_road r
 	where st_dwithin(q.geom::geography, r.geom::geography, 10)
+		and r.type not in ('service', 'footway', 'steps', 'track', 'cycleway', 'bridleway')
 	order by q.geom::geography <-> r.geom::geography
 	limit 1
 ) r on true
-where q.id_gis = 1051 -- дебаг
+--where q.id_gis = 1051 -- дебаг
 ;
+create index on tmp.quazar_on_track(street_id);
 
 -- Сборка статистики на графе
 drop table if exists tmp.quazar_graph;
@@ -101,12 +122,13 @@ select
 	avg(q.speed) filter(where extract(hour from q.date_time) in (7,8,9,10,18,19,20)) speed_rush_hour,
 	avg(q.speed) filter(where extract(hour from q.date_time) not in (7,8,9,10,18,19,20)) - avg(q.speed) filter(where extract(hour from q.date_time) in (7,8,9,10,18,19,20)) speed_dif
 from index2019.data_road r
-left join tmp.quazar_veb_on_track q using(street_id)
-where r.id_gis = 1051 -- дебаг
+left join tmp.quazar_on_track q using(street_id)
+--where r.id_gis = 1051 -- дебаг
+where r.id_gis in (select id_gis from veb_rf.city)
 group by r.street_id, r.id_gis,	r.name,	r.type, r.geom
 ;
 
-select date_time, extract(hour from date_time) hour_ from tmp.quazar_veb_on_track
+select date_time, extract(hour from date_time) hour_ from tmp.quazar_on_track
 	
-	
+create index on osm.extent using gist(geom)
 	
