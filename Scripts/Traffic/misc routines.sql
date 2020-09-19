4b2ff3a336b63fd20a290457520dba8dd63c46d8
FelisPimeja <felispimeja@gmail.com>
2020-09-20 00:07:32 +0300

    misc edits

--- Scripts/Traffic/misc routines.sql
+++ Scripts/Traffic/misc routines.sql
@@ -1,16 +1,40 @@
+-- Обработка первых "сырых" точек google traffic после загрузки в базу
+
 alter table traffic.google_initial
 	drop column "path";
 
 update traffic.google_initial
-	set layer = substring(layer, 4, length(layer))
+	set layer = substring(layer, 4, length(layer));
 
-select layer, substring(layer, 4, length(layer)) from traffic.google_initial limit 100
+alter table traffic.google_initial
+	alter column layer type smallint;
 
+alter table traffic.google_initial
+	rename column layer to cell_id;
+	
 create index on traffic.google_initial using gist(geom);
 create index on traffic.google_initial using gist((geom::geography));
 
-create temp table tmp as select * from traffic.google_initial limit 100;
-update tmp
-	set layer = substring(layer, 4, length(layer));
+alter table traffic.google_initial
+	rename column "VALUE" to "value";
 
-select * from tmp;
+create index on traffic.google_initial(value);
+
+alter table russia.rect_grid_tmp
+	set schema traffic;
+
+alter table traffic.rect_grid_tmp rename to grid_russia;
+
+alter table russia.rect_grid
+	set schema traffic;
+
+alter table traffic.rect_grid rename to grid_russia_city;
+
+alter table traffic.grid_russia_city
+	add column google_traffic bool default false;
+
+update traffic.grid_russia_city
+	set google_traffic = true 
+	where id in (select distinct cell_id from traffic.google_initial);
+
+select count(*) from traffic.grid_russia_city --where google_traffic is true
