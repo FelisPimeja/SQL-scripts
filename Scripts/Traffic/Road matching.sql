a8c6e53ffbe717660d5f6255bd16766583f3db6d
FelisPimeja <felispimeja@gmail.com>
2020-09-28 10:10:07 +0300

    misc edits

--- Scripts/Traffic/Road matching.sql
+++ Scripts/Traffic/Road matching.sql
@@ -8,27 +8,83 @@ alter table traffic.google_initial
 alter table traffic.google_initial
 	add column dist_to_road_m int;
 
-/* Матчинг */
-update traffic.google_initial i
-	set
-		road_segment_id = r.road_segment_id,
-		dist_to_road_m = r.dist_to_road_m 
-	from (
-		select
-			i.id,
-			r.id road_segment_id,
-			st_distance(i.geom::geography, r.geom::geography)::int dist_to_road_m,
-			i.geom
-		from traffic.google_initial i
-		left join lateral (
-			select r.id, r.geom
-			from traffic.city_road_rebuild r
-			where st_dwithin(i.geom::geography, r.geom::geography, 15)
-			order by i.geom::geography <-> r.geom::geography
-			limit 1
-		) r on true
-	) r
+create index on traffic.city_road_rebuild using gist((geom::geography));
+create index on traffic.google_initial(cell_id);
+
+/* Матчинг в новую таблицу потому что обновление traffic.google_initial проходит очень долго!!! */
+-- Время выполнения ~ 1 час + полчаса на  индексы
+drop table if exists traffic.google_matched;
+create table traffic.google_matched as 
+--explain
+select
+	i.id,
+	i.geom,
+	i.value,
+	i.cell_id,
+	r.id road_segment_id,
+	st_distance(i.geom::geography, r.geom::geography)::int dist_to_road_m	
+from traffic.google_initial i
+left join lateral (
+	select r.id, r.geom
+	from traffic.city_road_rebuild r
+	where st_dwithin(i.geom::geography, r.geom::geography, 15)
+	order by i.geom::geography <-> r.geom::geography
+	limit 1
+) r on true
+--where i.cell_id = 1
 ;
 
-create index on traffic.google_initial(road_segment_id);
-create index on traffic.google_initial(dist_to_road_m);
+-- PK и индексы
+alter table traffic.google_matched add primary key(id);
+create index on traffic.google_matched using gist(geom);
+create index on traffic.google_matched using gist((geom::geography));
+create index on traffic.google_matched(cell_id);
+create index on traffic.google_matched(value);
+create index on traffic.google_matched(road_segment_id);
+create index on traffic.google_matched(dist_to_road_m);
+
+
+/* Перенос медианного значения на рёбра графа */
+-- Время выполнения ~ 4 минут.
+-- To do:
+-- 1. Попробовать отсекать ложные срабатывания на примыкающих дорогах через расчёт плотности точек на км дороги
+
+drop table if exists traffic.graph_matched1;
+create table traffic.graph_matched1 as 
+select
+	r.*,
+	percentile_disc(0.5) within group(order by g.value) median_value
+from traffic.city_road_rebuild r
+left join traffic.google_matched g
+	on r.id = g.road_segment_id
+group by r.id, r.id_gis, r.cell_id, r.geom;
+
+alter table traffic.graph_matched1 add primary key(id);
+create index on traffic.graph_matched1(median_value);
+create index on traffic.graph_matched1(cell_id);
+create index on traffic.graph_matched1(id_gis);
+create index on traffic.graph_matched1 using gist(geom);
+create index on traffic.graph_matched1 using gist(((st_lineinterpolatepoint(geom, (0.5)::double precision))::geography));
+
+--select count(*) from traffic.graph_matched1 where median_value is not null
+
+
+/* Поиск одной точки для каждого ребра графа, по которой можно собирать загруженность */
+-- Время выполнения ~ 10 минут.
+drop table if exists traffic.graph_match_points;
+create table traffic.graph_match_points as 
+select
+	r.id road_segment_id,
+	g.geom
+from traffic.graph_matched1 r
+join lateral (
+	select g.road_segment_id, g.geom
+	from traffic.google_matched g
+	where r.id = g.road_segment_id
+		and r.median_value = g.value 
+	order by g.geom::geography <-> (st_lineinterpolatepoint(r.geom, (0.5)::double precision))::geography
+	limit 1
+) g on true 
+
+alter table traffic.graph_match_points add primary key(road_segment_id);
+create index on traffic.graph_match_points using gist(geom);
--- Scripts/misc scripts.sql
+++ Scripts/misc scripts.sql
@@ -27,5 +27,101 @@ where b.city in (
 );
 
 
+-- Статистика по плотности населения в урбанизированной части городов + ещё несколько показателей
+-- Время расчёта ~ 30 сек.
+with public_transport_stops as (
+	select 
+		id_gis,
+		count(*) pt_stop_count
+	from index2019.data_poi 
+	where rubrics = 'Остановка общественного транспорта'
+	group by id_gis
+),
 
+dist_1m_city as (
+	select 
+		b1.id_gis,
+		b1.city,
+		b1.geog,
+		b2.id_gis id_gis2,
+		b2.city,
+		round((st_distance(b1.geog, b2.geog))::numeric / 1000) dist_1m_city
+	from (
+		select
+			c.id_gis,
+			c.city,
+			st_centroid(c.geom)::geography geog,
+			p.pop2020 pop
+		from russia.city c
+		left join index2020.data_pop_rosstat p using(id_gis)
+	) b1
+	left join lateral (
+		select
+			b2.id_gis,
+			b2.city,
+			b2.geog
+		from (
+			select
+				c.id_gis,
+				c.city,
+				st_centroid(c.geom)::geography geog,
+				p.pop2020 pop
+			from russia.city c
+			left join index2020.data_pop_rosstat p using(id_gis)
+		) b2
+			where b2.pop >= 1000000
+		order by b1.geog <-> b2.geog
+		limit 1
+	) b2 on true
+	where b1.geog is not null
+),
 
+sdz_density as (select id_gis, count(*) filter(where sdz is true) count_sdz	from index2019.data_poi	group by id_gis)
+
+select
+	c.id_gis,
+	c.city "Город",
+	c.region_name "Субъект РФ",
+--	c.start_year "Год основания",
+	round((st_area(c.geom::geography) / 10000)::numeric, 2) "Площадь города в границах, га",
+	ba.area_ha "Площадь урбан. территории, га",
+	p.pop2020 "Нас. по данным Росстата на 2020 г., чел.",
+	coalesce((p.pop2020 / ba.area_ha)::int, 0) "Плотн. нас. урбан. части, чел./га",
+	round((sd.count_sdz / (st_area(c.geom::geography) / 10000))::numeric, 2) "Плотность соц-досуг функций, ед./га",
+	pt.pt_stop_count "Кол-во остановок общ. транспорта",
+	d.dist_1m_city "Расстояние до ближ. миллионника, км.",
+	coalesce(g.type, 'Нет') "Геостратегическая территория РФ",
+	case when c.start_year < 1917 then 'Есть' else 'Нет' end "Наличие исторического центра"
+from russia.city c
+left join russia.city_built_area_light ba using(id_gis)
+left join index2020.data_pop_rosstat p using(id_gis)
+left join public_transport_stops pt using(id_gis)
+left join dist_1m_city d using(id_gis)
+left join sdz_density sd using(id_gis)
+left join russia.geostrategic_territory g
+	on st_intersects(g.geom, st_centroid(c.geom))
+where c.geom is not null
+order by p.pop2020 desc;
+
+alter table traffic.collect_data_test2 drop constraint collect_data_test2_pkey
+
+drop table if exists tmp.rast_test;
+create table tmp.rast_test as select t1.rid, t1.filename, st_clip(t1.rast, 1, t3.geom, -999, true) rast from tmp.r_test1 t1 join tmp.t3 on st_intersects(t3.geom, t1.rast)
+
+drop table tmp.cycle_rout_cadaster; 
+create table tmp.cycle_rout_cadaster as 
+select distinct on (p.id) p.* from cadastr2016.parcel p join tmp.route c on st_intersects(p.geom, c.buf);
+create index on tmp.cycle_rout_cadaster using gist(geom);
+alter table tmp.cycle_rout_cadaster add primary key(id);
+create index on tmp.cycle_rout_cadaster(cad_ref);
+
+alter table tmp.route add column buf geometry(polygon, 4326);
+update  tmp.route set buf = st_buffer(geom::geography, 50)::geometry(polygon, 4326);
+create index on  tmp.route using gist(buf);
+
+create index on tmp.corridor using gist(geom);
+
+alter table traffic.city_road_rebuild
+	rename column id to road_segment_id
+	
+	
