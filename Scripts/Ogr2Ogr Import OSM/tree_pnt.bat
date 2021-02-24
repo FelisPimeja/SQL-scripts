cd59a53c07cdc4cdc16db6f9af1efe8b9b022648
Felis Pimeja <felispimeja@gmail.com>
2021-02-24 19:14:48 +0300

    resolving conflicts

--- Scripts/Ogr2Ogr Import OSM/tree_pnt.bat
+++ Scripts/Ogr2Ogr Import OSM/tree_pnt.bat
@@ -1,6 +1,5 @@
 set startTime=%time%
 :: Загрузка точечных деревьев
-<<<<<<< HEAD
 :: Время выполнения ~ 7 мин.
 :: todo - прогнать и замерить время проверить ссылки на wiki
  ogr2ogr ^
@@ -43,47 +42,3 @@ comment on column russia.tree_pnt_osm.id_gis is 'id_gis города. Внешн
 
 echo Загрузка Точечных деревьев Начало: %startTime%
 echo Загрузка Точечных деревьев Завершение: %time%
-=======
-:: Время выполнения ~  мин.
-:: todo - прогнать и замерить время проверить ссылки на wiki
- ogr2ogr ^
- -f PostgreSQL PG:"dbname=kbpvdb user=editor password=pgeditor host=gisdb.strelkakb.ru port=5433" ^
- "D:\apetrov\Projects\Postgres\OSM\PBF\russia-latest.osm.pbf" ^
- -sql "select null id_gis, other_tags, geometry from points where natural = 'tree' " ^
- --config OSM_CONFIG_FILE "D:\apetrov\Projects\Postgres\OSM\Osmconf\osmconf.ini" ^
- --config PG_USE_COPY YES ^
- --config MAX_TMPFILE_SIZE 2048 ^
- -nln russia.tree_pnt_osm ^
- -nlt POINT ^
- -lco GEOMETRY_NAME=geom ^
- -lco SPATIAL_INDEX=NONE ^
- -lco COLUMN_TYPES=other_tags=hstore,id_gis=smallint ^
- -lco FID=id ^
- -dialect SQLite ^
- -overwrite
-
-:: Приведение, обработка, индексы и комментарии
-ogr2ogr ^
- PostgreSQL PG:"dbname=kbpvdb user=editor password=pgeditor host=gisdb.strelkakb.ru port=5433" ^
- -sql ^
-"/* Проверка геометрии, id_gis и площади */ ^
-update russia.tree_pnt_osm set geom = st_snaptogrid(geom, 0.0000001); ^
-delete from russia.tree_pnt_osm where st_isempty(geom) is true; ^
-alter table russia.tree_pnt_osm add constraint fk_id_gis foreign key(id_gis) references russia.city(id_gis); ^
-create index on russia.tree_pnt_osm using gist(geom);^
-update russia.tree_pnt_osm b set id_gis = bn.id_gis from russia.city bn where st_within(b.geom, bn.geom); ^
-/* Индексы */ ^
-create index on russia.tree_pnt_osm(id_gis); ^
-create index on russia.tree_pnt_osm using gin(other_tags); ^
-create index tree_pnt_osm_geog_idx on russia.tree_pnt_osm using gist((geom::geography)); ^
-/* Комментарии */ ^
-comment on table russia.tree_pnt_osm is 'Точечные деревья (OpenStreetMap). Актуальность - %date%'; ^
-comment on column russia.tree_pnt_osm.id is 'Первичный ключ'; ^
-comment on column russia.tree_pnt_osm.other_tags is 'Прочие теги'; ^
-comment on column russia.tree_pnt_osm.geom is 'Геометрия'; ^
-comment on column russia.tree_pnt_osm.id_gis is 'id_gis города. Внешний ключ';"
-
-
-echo Загрузка Дорог Начало: %startTime%
-echo Загрузка Дорог Завершение: %time%
->>>>>>> branch 'master' of https://github.com/FelisPimeja/SQL-scripts.git
